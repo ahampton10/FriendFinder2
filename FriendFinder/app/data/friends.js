07035d82208a674d74a7b333aa26a4e7f88883e1
Alex H <hampton.alex10@gmail.com>
2020-01-17 12:49:54 -0700

    still no

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,16 @@
+var friendList = [
+    {
+      name: 'Justin Bieber',
+      profilePic: 'https://www.wthr.com/sites/default/files/styles/article_image/public/2019/05/03/justin_bieber_cropped.jpg?itok=oQ8KPFhI',
+      scores: [ 4, 5, 5, 2, 4, 5, 2, 2, 1, 3] 
+    
+  },
+  {
+     name: 'Guy Fieri',
+     profilePic: "https://imgix.bustle.com/rehost/2016/9/13/675c9c26-713a-4743-b263-8efff6d12bdd.jpghttps://media.gq.com/photos/59dfc6d9d61cb80476584e18/master/pass/guy-fieiri-flame.jpg",
+     scores: [3, 5, 5, 2, 1, 4, 5, 3, 2, 2]
+  }
+  ]; 
+  
+  module.exports = friendList;
+    
